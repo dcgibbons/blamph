997f493ca8bce7657ceee60db1a86f75dac1435c
Chad Gibbons <dcgibbons@gmail.com>
2012-11-15 18:46:17 -0700

    better auto layout constraints; improved status bar layout; initial attempt at draggable window sections; fixes #6

---
+++ Blamph/StatusBarView.m
@@ -0,0 +1,19 @@
+//
+//  StatusBarView.m
+//  Blamph
+//
+//  Created by Chad Gibbons on 11/15/12.
+//  Copyright (c) 2012 Nuclear Bunny Studios, LLC. All rights reserved.
+//
+
+#import "StatusBarView.h"
+
+@implementation StatusBarView
+
+- (BOOL)mouseDownCanMoveWindow
+{
+    NSLog(@"mouseDownCanMoveWindow!");
+    return YES;
+}
+
+@end
