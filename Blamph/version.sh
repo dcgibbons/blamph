cb4a7f9525d6f32980b0017dcc121ee5eafacff0
Chad Gibbons <dcgibbons@gmail.com>
2012-11-20 00:14:10 -0700

    remove whitespace from git version values

--- Blamph/version.sh
+++ Blamph/version.sh
@@ -4,8 +4,8 @@ set -x
 
 INFO_PLIST="${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}"
 
-git_build=`git rev-list HEAD| wc -l`
-git_version=`git describe --dirty | sed -e 's/^v//' -e 's/g//'`
+git_build=`git rev-list HEAD| wc -l| tr -d ' '`
+git_version=`git describe --dirty | sed -e 's/^v//' -e 's/g//'| tr -d ' '`
 
 defaults write "${INFO_PLIST%.*}" CFBundleVersion "${git_build}"
 defaults write "${INFO_PLIST%.*}" CFBundleShortVersionString "${git_version}"
